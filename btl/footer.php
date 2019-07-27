77fd742d840b9669d3f51b32cbe96296e9b3e12e
= <=>
2019-07-27 22:18:51 +0700

    bai tap lon

---
+++ btl/footer.php
@@ -0,0 +1,46 @@
+<footer class="footer">
+			<div class="container">
+				<div class="it-nav-footer">
+					<ul class="nav-footer-link">
+						<li> <a href="#" class="ui-link-white"> Giới thiệu</a></li>
+						<li> <a href="#" class="ui-link-white"> Logo của khoa CNTT</a></li>
+						<li> <a href="#" class="ui-link-white"> Lời chào mừng</a></li>
+						<li> <a href="#" class="ui-link-white"> Tổ chức</a></li>
+						<li> <a href="#" class="ui-link-white"> Hợp tác liên kết</a></li>
+					</ul>
+					<ul class="nav-footer-link">
+						<li> <a href="#" class="ui-link-white"> Đào tạo</a></li>
+						<li> <a href="#" class="ui-link-white"> Mô hình đào tạo</a></li>
+						<li> <a href="#" class="ui-link-white"> Đào tạo đại học</a></li>
+						<li> <a href="#" class="ui-link-white"> Định hướng ngành nghề</a></li>
+						<li> <a href="#" class="ui-link-white"> Đào tạo sau đại học</a></li>
+					</ul>
+					<ul class="nav-footer-link">
+						<li> <a href="#" class="ui-link-white"> Nghiên cứu khoa học</a></li>
+						<li> <a href="#" class="ui-link-white"> Thông tin seminar</a></li>
+						<li> <a href="#" class="ui-link-white"> Các đề tài, dự án</a></li>
+						<li> <a href="#" class="ui-link-white"> CÔng trình công bố</a></li>
+						<li> <a href="#" class="ui-link-white"> Các phòng thí nghiệm</a></li>
+					</ul>
+					<ul class="nav-footer-link">
+						<li> <a href="#" class="ui-link-white"> Công nghệ phần mềm</a></li>
+						<li> <a href="#" class="ui-link-white"> Hệ thống thông tin</a></li>
+						<li> <a href="#" class="ui-link-white"> Khoa học máy tính</a></li>
+						<li> <a href="#" class="ui-link-white"> Kỹ thuật máy tính và mạng</a></li>
+						<li> <a href="#" class="ui-link-white"> Toán học</a></li>
+					</ul>
+				</div>
+				<div class="footer_bot">
+					<img src="img/img-01.png" alt="" class="footer_bot-img">
+					<p>© 2017 Khoa Công nghệ thông tin - Đại học Thủy lợi <br> 
+             		   Địa chỉ: nhà C1, Đại học Thủy lợi, 175 Tây Sơn, Đống Đa, Hà Nội. Điện thoại: (+84)-024 3 5632211. Email: 
+             		   <a href="#" title="" class="ui-link-white">vpkcntt@tlu.edu.vn</a>
+					</p>
+				</div>
+				<div class="clear"></div>
+			</div>
+		</footer>
+	</div>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
