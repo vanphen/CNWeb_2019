83eda52bda3f404ecb2098fd7e8bab88029bf926
= <=>
2019-07-27 22:02:33 +0700

    bai tap lon

---
+++ btl/header.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+		<meta name="description" content="Use our powerful mobile-first flexbox grid to build layouts of all shapes and sizes thanks to a twelve column system, five default responsive tiers, Sass variables and mixins, and dozens of predefined classes.">
+	    <!-- Bootstrap CSS -->
+	    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+		<meta http-equiv="content-language" itemprop="inLanguage" content="vi">
+		<meta name="keywords" itemprop="keywords" content="">
+		<link rel="stylesheet" href="css/style.css">
+		<title> Khoa công nghệ thông tin đại học Thủy Lợi </title>
+</head>
+<body>
+	<div class="wrapper">
+		<header id="header" class="header">
+			<div id="topHeader">
+				<div class="globalnav">
+					<div class="container">
+						<div class="header_top">
+							<div class="lable">
+								<a href="#" class="ui-link-white"> Trường Đại học Thủy Lợi - TLU</a>
+							</div>
+							<!-- class lable -->
+							<ul class="list-top">
+								<li>(+) | </li>
+								<li> <a href="#">Login</a></li>
+								<li>|</li>
+								<li> <a href="#"> Register</a></li>
+								<li>|</li>
+								<li class="language">
+									<span class="language-text"> Ngôn ngữ: </span>
+									<a href="#" title="Tiếng Việt" class="ui-link-white lang-vi">
+										<img src="img/icon-lang-vi.png" alt="">
+									</a>
+									<a href="#" title="Tiếng Anh" class="ui-link-white lang-en">
+										<img src="img/icon-lang-en.png" alt="">
+									</a>
+								</li>
+							</ul>
+							<div class="container-search"> 
+								<div class="dnn_Search">
+									<div class="viewsearch">
+										<i class="ico-search"></i>
+										<input type="text" class="input-text" placeholder="Tim Kiem" autocomplete="off">
+										<input type="image" class="searchbutton" value="tim" src="img/search-icon2.png" width="20px;">
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- class header_top -->
+					</div>
+					<!-- class container -->
+				</div>
+				<!-- class globalnav -->
+			</div>
+			<div class="container">
+				<div class="header_logo">
+					<h1>
+						<a href="#">
+							<img src="img/logo.jpg" width="547px"; height="82px" alt="">
+						</a>
+					</h1>
+				</div>
+				<div class="btn-toggle">
+					<a href="#" class="toggle-nav" title="Toggle-nav">
+						<span> Toggle Nav</span>
+					</a>
+				</div>
+			</div>
+		</header><!-- /header -->
+		<div class="container">
+			<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav mr-auto">
+						<li class="nav-item active">
+			        		<a class="nav-link" href="#" title="Trang chủ">TRANG CHỦ<span class="sr-only">(current)</span></a>
+			      		</li>
+			      		<li class="nav-item dropdown">
+        					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        					  	GIỚI THIỆU
+        					</a>
+					        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+						        <a class="dropdown-item" href="#">Logo khoa CNTT</a>
+					          	<a class="dropdown-item" href="#">Lời chào mừng</a>
+					          	<a class="dropdown-item" href="#">Tổ chức</a>
+					          	<a class="dropdown-item" href="#">Hợp tác liên kếtc</a>
+					        </div>
+      					</li>
+			      		<li class="nav-item dropdown">
+        					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        					  	NGHIÊN CỨU KHOA HỌC
+        					</a>
+					        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+						        <a class="dropdown-item" href="#">Các đề tài, dự án</a>
+					          	<a class="dropdown-item" href="#">Thông tin seminar</a>
+					          	<a class="dropdown-item" href="#">Công trình công bố</a>
+					          	<a class="dropdown-item" href="#">Các phòng thí nghiệm</a>
+					        </div>
+      					</li>
+			      		<li class="nav-item dropdown">
+        					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        					  	ĐÀO TẠO
+        					</a>
+					        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+						        <a class="dropdown-item" href="#">Đào tạo đại học</a>
+					          	<a class="dropdown-item" href="#">Đào tạo sau đại học</a>
+					          	<a class="dropdown-item" href="#">Chuẩn đầu ra</a>
+					          	<a class="dropdown-item" href="#">Định hướng ngành nghề</a>
+					          	<a class="dropdown-item" href="#">Mô hình đào tạo</a>
+					          	<a class="dropdown-item" href="#">Đề cương môn học</a>
+					        </div>
+      					</li>
+			     		<li class="nav-item dropdown">
+        					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        					  	BỘ MÔN - TRUNG TÂM
+        					</a>
+					        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+						        <a class="dropdown-item" href="#">CN phần mềm</a>
+					          	<a class="dropdown-item" href="#">Hệ thống thông tin</a>
+					          	<a class="dropdown-item" href="#">Khoa học máy tính</a>
+					          	<a class="dropdown-item" href="#">Kỹ thuật máy tính và mạng</a>
+					          	<a class="dropdown-item" href="#">Toán học</a>
+					          	<a class="dropdown-item" href="#">Tin học và kỹ thuật tính toán</a>
+					          	<a class="dropdown-item" href="#">Trung tâm tin học</a>
+					        </div>
+      					</li>
+			     		<li class="nav-item dropdown">
+        					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        					  	SINH VIÊN
+        					</a>
+					        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+						        <a class="dropdown-item" href="#">Thông báo</a>
+					          	<a class="dropdown-item" href="#">TB đào tạo</a>
+					          	<a class="dropdown-item" href="#">Nghiên cứu khoa học</a>
+					          	<a class="dropdown-item" href="#">Tuyển dụng</a>
+					          	<a class="dropdown-item" href="#">Học bổng</a>
+					          	<a class="dropdown-item" href="#">Thông báo khác</a>
+					        </div>
+      					</li>
+			     		<li class="nav-item dropdown">
+        					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        					  	TIN TỨC
+        					</a>
+					        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+						        <a class="dropdown-item" href="#">Sự kiện</a>
+					          	<a class="dropdown-item" href="#">CSE trên báo</a>
+					        </div>
+      					</li>
+			     		<li class="nav-item dropdown">
+        					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        					  	THÔNG BÁO
+        					</a>
+					        <div class="dropdown-menu">
+						        <a class="dropdown-item" href="#">Logo khoa CNTT</a>
+					          	<a class="dropdown-item" href="#">Lời chào mừng</a>
+					          	<a class="dropdown-item" href="#">Tổ chức</a>
+					          	<a class="dropdown-item" href="#">Hợp tác liên kếtc</a>
+					        </div>
+      					</li>
+			     		<li class="nav-item">
+			        		<a class="nav-link disabled" href="#" title="Liên hệ">LIÊN HỆ</a>
+			     		</li>
+			    	</ul>
+			 	</div>
+			</nav>
---
+++ btl/trangchu.php
@@ -0,0 +1,200 @@
+<?php require_once "header.php";?>
+	<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+			  	<ol class="carousel-indicators">
+			    	<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+			    	<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+			    	<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+			    	<li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+			    	<li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+			    	<li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+			    	<li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
+			 	</ol>
+				<div class="carousel-inner">
+				    <div class="carousel-item active">
+				    	<img class="d-block w-100" src="img/sanh-t54.jpg"">
+				    </div>
+				    <div class="carousel-item">
+				      	<img class="d-block w-100" src="img/rivf-gala.jpg">
+				    </div>
+				    <div class="carousel-item">
+				      	<img class="d-block w-100" src="img/khai-truong.jpg">
+				    </div>
+				    <div class="carousel-item">
+				      	<img class="d-block w-100" src="img/banner.jpg">
+				    </div>
+				    <div class="carousel-item">
+				      	<img class="d-block w-100" src="img/bannertq4.jpg">
+				    </div>
+				    <div class="carousel-item">
+				      	<img class="d-block w-100" src="img/hoi-thao.jpg">
+				    </div>
+				    <div class="carousel-item">
+				      	<img class="d-block w-100" src="img/cse-tlu-nara.jpg">
+				    </div>
+				</div>
+			  	<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+			    	<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			    	<span class="sr-only">Previous</span>
+			  	</a>
+			  	<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+			    	<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			    	<span class="sr-only">Next</span>
+			  	</a>
+			</div>
+		</div>
+		<section class="main">
+			<div class="container">
+				<div class="row">
+					<div id="dnn_ContentPane" class="col-md-12">
+						<div class="row row-white">
+							<div class="col-md-9 spacing top">
+								<div class="portlet-column span9">
+									<div class="portlet-dropzone">
+										<div class="portlet-boundary">
+											<section class="portlet">
+												<header class="portlet-topper">
+													<div class="portlet-fulltitle">
+														<h1 class="portlet-title">
+															<span class="portlet-title-text" data="Tin Tức">
+																Tin tức
+															</span>
+														</h1>
+													</div>
+												</header>
+												<div class="portlet-content">
+													<div class="portlet-content-container">
+														<div class="new-wrapper">
+															<div class="cse.tlu-tintuc">
+																<div class="row">
+																	<div class="col-md-4 col-sm-6 col-12">
+																		<div class="card-product">
+																			<a href="#"><img class="card-img-top" src="img/cse-hallthumb.jpg" alt="Card image cap"> </a>
+																			<div class="card-body">
+							    												<div class="title">
+																					<a href="#"> 
+																					Lời chào mừng
+																					</a>
+																				</div>
+																			</div>
+																		</div>
+																	</div>
+																	<div class="col-md-4 col-sm-6 col-12">
+																		<div class="card-product">
+																			<a href="#"><img class="card-img-top" src="img/gv-khoa-cnttthumb.jpg" alt="Card image cap"> </a>
+																			<div class="card-body">
+							    												<div class="title">
+																					<a href="#"> 
+																					Nghiên cứu khoa học
+																					</a>
+																				</div>
+																			</div>
+																		</div>
+																	</div>
+																	<div class="col-md-4 col-sm-6 col-12">
+																		<div class="card-product">
+																			<a href="#"><img class="card-img-top" src="img/cse-tlu-narathumb.jpg" alt="Card image cap"> </a>
+																			<div class="card-body">
+							    												<div class="title">
+																					<a href="#"> 
+																					Giảng viên
+																					</a>
+																				</div>
+																			</div>
+																		</div>
+																	</div>
+																	<!-- md 4 -->
+																	<div class="col-md-4 col-sm-6 col-12">
+																		<div class="card-product">
+																			<a href="#"><img class="card-img-top" src="img/k54th-tot-nghiep-1thumb.jpg" alt="Card image cap"> </a>
+																			<div class="card-body">
+							    												<div class="title">
+																					<a href="#"> 
+																					Đào tạo
+																					</a>
+																				</div>
+																			</div>
+																		</div>
+																	</div>
+																	<!-- md 4 -->
+																	<div class="col-md-4 col-sm-6 col-12">
+																		<div class="card-product">
+																			<a href="#"><img class="card-img-top" src="img/khoa43th.jpg" alt="Card image cap"> </a>
+																			<div class="card-body">
+							    												<div class="title">
+																					<a href="#"> 
+																					Ảnh khoa CNTT
+																					</a>
+																				</div>
+																			</div>
+																		</div>
+																	</div>
+																	<!-- md 4 -->
+																	<div class="col-md-4 col-sm-6 col-12">
+																		<div class="card-product">
+																			<a href="#"><img class="card-img-top" src="img/cnpm.jpg" alt="Card image cap"> </a>
+																			<div class="card-body">
+							    												<div class="title">
+																					<a href="#"> 
+																					Ngành kỹ thuật phần mềm
+																					</a>
+																				</div>
+																			</div>
+																		</div>
+																	</div>
+																	<!-- md 4 -->
+																</div>
+																<!-- div rơ -->
+															</div>
+														</div>
+													</div>
+												</div>
+												<!-- div portlet-content -->
+											</section>
+										</div>
+									</div>
+								</div>
+								<!-- span 9 -->
+							</div>
+							<div class="portlet-column col-md-3">
+								<section class="portlet">
+									<header class="portlet-topper">
+										<div class="portlet-fulltitle">
+											<h1 class="portlet-title">
+												<span class="portlet-title-text" data="Tin nổi bật">Tin nổi bật</span>
+											</h1>
+										</div>
+									</header>
+									<div class="portlet-content">
+										<div class="cse-tlu-tinnoibat">
+											<div class="portlet-content-container">
+												<ul class="news-ul3-Ex">
+													<li class="tnb-first ">
+														<a href="#" title=" Chương trình kỹ sư công nghệ làm việc tại Nhật Bản năm 2019">Chương trình kỹ sư công nghệ làm việc tại Nhật Bản năm 2019 </a>
+													</li>
+												</ul>
+												<ul class="news-ul3-Ex">
+													<li class="tnb-first ">
+														<a href="#" title=" Chương trình kỹ sư công nghệ làm việc tại Nhật Bản năm 2019">Chương trình kỹ sư công nghệ làm việc tại Nhật Bản năm 2019 </a>
+													</li>
+												</ul>
+												<ul class="news-ul3-Ex">
+													<li class="tnb-first ">
+														<a href="#" title=" Chương trình kỹ sư công nghệ làm việc tại Nhật Bản năm 2019">Chương trình kỹ sư công nghệ làm việc tại Nhật Bản năm 2019 </a>
+													</li>
+												</ul>
+												<a href="#" title="" class="icon-detail"> >>>Xem tất cả</a>
+											</div>	
+										</div>
+									</div>
+								</section>
+							</div>
+							<!-- span 3 -->
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- <div class="container">
+				<img src="img/sile1.jpg" alt="">
+			</div> -->
+		</section> <!-- main -->
+<?php require_once "footer.php";?>
