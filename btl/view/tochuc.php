9a857031e205e87ba4820f016640112ae977329a
= <=>
2019-08-05 13:10:08 +0700

    bai tap lon

---
+++ btl/view/tochuc.php
@@ -0,0 +1,287 @@
+<?php require_once "header.php";?>
+	<div class="location">
+		<div class="container breadcrumb">
+			<a href="gioithieu.php" title="" class="breadcrumblink"> 
+				<span> Giới thiệu   >   </span>
+			</a>
+			<a href="loichao.php" title="" class="breadcrumblink">
+				<span> Lời chào mừng</span>
+			</a>
+		</div>
+		<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+			  	<ol class="carousel-indicators">
+			    	<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+			    	<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+			    	<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+			    	<li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+			    	<li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+			    	<li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+			    	<li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
+			 	</ol>
+				<div class="carousel-inner">
+				    <div class="carousel-item active">
+				    	<img class="d-block w-100" src="../public/img/sanh-t54.jpg">
+				    </div>
+				    <div class="carousel-item">
+				      	<img class="d-block w-100" src="../public/img/rivf-gala.jpg">
+				    </div>
+				    <div class="carousel-item">
+				      	<img class="d-block w-100" src="../public/img/khai-truong.jpg">
+				    </div>
+				    <div class="carousel-item">
+				      	<img class="d-block w-100" src="../public/img/banner.jpg">
+				    </div>
+				    <div class="carousel-item">
+				      	<img class="d-block w-100" src="../public/img/bannertq4.jpg">
+				    </div>
+				    <div class="carousel-item">
+				      	<img class="d-block w-100" src="../public/img/hoi-thao.jpg">
+				    </div>
+				    <div class="carousel-item">
+				      	<img class="d-block w-100" src="../public/img/cse-tlu-nara.jpg">
+				    </div>
+				</div>
+			  	<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+			    	<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			    	<span class="sr-only">Previous</span>
+			  	</a>
+			  	<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+			    	<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			    	<span class="sr-only">Next</span>
+			  	</a>
+			</div>
+		<div class="inner-page">
+			<main class="main">
+				<div class="container">
+					<div class="maincontainer-inner">
+						<div class="row">
+							<div id="dnn_ContentPane" class="col-md-12">
+								<div class="row row-white">
+									<div class="col-md-9 anh ">
+										<h2 class="title"> Sơ đồ tổ chức khoa Công nghệ thông tin</h2>
+										<div class="description">
+											<p style="text-align: justify">
+												<img src="../public/img/so do khoa cntt.jpg" alt="">
+											</p>
+											<p  style="text-align: justify">
+												<strong>
+													* Ban chủ nhiệm khoa
+												</strong>
+											</p>
+											<p  style="text-align: justify;width: 300px;height: 200px;">
+												<img src="../public/img/avar1.jpg" alt="">
+											</p>
+											<p> - Trưởng khoa: TS. <a href="#" title="" style="color: blue;"> Nguyễn Thanh Tùng</a></p>
+											<p> Email: tungnt@tlu.edu.vn     Điện thoại: +84-4-38521442 </p>
+											<p> TS. Nguyễn Thanh Tùng phụ trách chung và trực tiếp phụ trách các công tác:</p>
+											<p> 
+												1.     Tổ chức, nhân sự 
+												<br>
+												2.     Đào tạo đại học và SĐH
+												<br>												
+												3.     Công tác hợp tác quốc tế
+												<br>
+												4.     Quy hoạch phát triển, kế hoạch
+												<br>
+												5.     Theo dõi vận hành, cập nhật thông tin Website khoa CNTT (CSE)
+												<br>												
+												6.     Các Hội đồng (Khoa học, Tư vấn, ...)
+											</p>
+											<p> <span> Đầu mối làm việc với các phòng: TCCB, Đào tạo ĐH&SĐH, Tài vụ, HTQT, HCTH,  </span> </p>
+											<p>
+												<img src="../public/img/dongpx.jpg" alt="">
+											</p>
+											<p>
+												- Phó trưởng khoa: ThS. GVC. 
+												<a href="#"> Phạm Xuân Đồng </a>
+											</p>
+											<p> Email: dongpx@tlu.edu.vn </p>
+											<p><span> ThS. Phạm Xuân Đồng trực tiếp phụ trách các công tác sau: </span></p>
+											<p>
+												1.      Công tác đoàn, hội sinh viên, công tác cựu sinh viên
+												<br>
+												2.      Xử lý học tập của sinh viên
+												<br>
+												3.      Cố vấn học tập
+												<br>	
+												4.      Theo dõi công tác phát triển giáo trình, sách tham khảo.
+												<br>
+												5.      Công tác thi đua, khen thưởng
+											</p>
+											<p>
+												Đầu mối làm việc với các phòng: CTCT&QLSV, Thư viện, HCTH, 
+												<span> Quản trị, Khảo thí & KĐCL </span>
+											</p>
+											<div>
+												<p>
+													<img src="../public/img/Copy of Dang Thu Hien.jpg" alt="" style="height: 179px; width: 120px;">
+												</p>
+												<p>
+													- Phó trưởng khoa: TS. GVC. 
+													<a href="#"> Đặng Thị Thu Hiền </a>
+												</p>
+												<p> Email: hiendt@tlu.edu.vn </p>
+												<p>
+													1.      Công tác sinh viên NCKH, Olympic sinh viên
+													<br>
+													2.      Quản trị cơ sở vật chất, các Dự án đầu tư
+													<br>
+													3.      Nghiên cứu khoa học, theo dõi thực hiện các đề tài nghiên cứu khoa học
+													<br>
+													4.      Phát triển quan hệ doanh nghiệp
+													<br>
+													5.      Kiểm định chất lượng
+													<br>
+													6.      Công tác tuyển sinh
+													<br>
+													7.      Hỗ trợ Trưởng khoa trong công tác Đào tạo đại học và SĐH
+												</p>
+												<p> Đầu mối làm việc: Phòng CTCT&QLSV, Phòng quản lý TB&ĐT, Phòng KHCN, Phòng HCTH </p>
+												<table border="0" cellpadding="0">
+													<tbody>
+														<tr>
+															<td rowspan="2"> 
+																<p> *Văn phòng khoa </p>
+															</td>
+															<td>
+																<p>  ThS. Nguyễn Thị Thu Hương </p>
+															</td>
+														</tr>
+														<tr>
+															<td>
+																<p>   KS. Nguyễn Khánh Linh </p>
+															</td>
+														</tr>
+													</tbody>
+												</table>
+												<p>
+													<strong> * Khối các đơn vị giảng dạy </strong> 
+												</p>
+											</div>
+											<table>
+												<tbody>
+													<tr>
+														<td>
+															<p>
+																<span style="font-size:larger;"> 
+																	<a href="#" title="">  - Bộ môn Công nghệ phần mềm </a>
+																</span>
+															</p>
+														</td>
+														<td>
+															<p>
+																<span style="font-size:larger;"> 
+																	Trưởng BM - TS. Lý Anh Tuấn 
+																</span>
+															</p>
+														</td>
+													</tr>
+													<tr>
+														<td>
+															<p>			
+																<span style="font-size:larger;"> 
+																	<a href="#" title="">  - Bộ môn Hệ thống thông tin </a>
+																</span>
+															</p>
+														</td>
+														<td>
+															<p>
+																<span style="font-size:larger;"> 
+																	Trưởng BM - TS.GVC Đặng Thị Thu Hiền
+																</span>
+															</p>
+														</td>
+													</tr>
+													<tr>
+														<td>
+															<span style="font-size:larger;"> 
+																<a href="#" title="">  - Bộ môn Khoa học máy tính </a>
+															</span>
+														</td>
+														<td>
+															<span style="font-size:larger;"> 
+																	Trưởng BM - TS. Nguyễn Ngọc Doanh
+															</span>
+														</td>
+													</tr>
+													<tr>
+														<td>
+															<span style="font-size:larger;"> 
+																<a href="#" title="">  - Bộ môn kỹ thuật máy tính và mạng </a>
+															</span>
+														</td>
+														<td>
+															<span style="font-size:larger;"> 
+																P. Trưởng BM - TS Phạm Tuấn Minh, ThS. Phạm Thanh Bình
+															</span>
+														</td>
+													</tr>
+													<tr>
+														<td>
+															<span style="font-size:larger;"> 
+																<a href="#" title="">  - Bộ môn tin học và kỹ thuật tính toán </a>
+															</span>
+														</td>
+														<td>
+
+															<span style="font-size:larger;"> 
+																Trưởng BM - TS Nguyễn Quỳnh Diệp
+															</span>
+														</td>
+													</tr>
+													<tr>
+														<td>
+															<span style="font-size:larger;"> 
+																<a href="#" title="">  - Bộ môn toán học </a>
+															</span>
+														</td>
+														<td>
+															<span style="font-size:larger;"> 
+																Trưởng BM- ThS. Phạm Xuân Đồng
+															</span>
+														</td>
+													</tr>
+												</tbody>
+											</table>
+										</div>
+										<div class="unitily clearfix">
+											<a href="loichao.php" title="">
+												<span class="toppage">
+													<u> Trở về đầu trang</u>
+												</span>
+											</a>
+										</div> 
+									</div>
+									<div class="protlet-colum col-md-3">
+										<div class="tlu-menu">
+											<p class="tlu-title">
+												<a href="gioithieu.php" title=""> Giới thiệu</a> 
+											</p>
+											<div class="column-2">
+												<ul>
+													<li>
+													 <a href="logo.php" title=""> > Logo khoa CNTT</a>
+													</li>
+													<li> 
+														<a href="loichao.php" title=""> > Lời chào mừng</a>
+													</li>
+													<li>
+													 <a href="tochuc.php" title=""> > Tổ chức</a>
+													</li>
+													<li> 
+														<a href="hoptac.php" title=""> > Hợp tác liên kết</a>
+													</li>
+												</ul>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</main>
+		</div>
+	</div>
+</div>
+<?php require_once "footer.php";?>
