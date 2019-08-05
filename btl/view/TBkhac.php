a00fc3d9cd664ebb63d6adb87fe4445ccc04bc75
= <=>
2019-08-05 22:29:33 +0700

    bai tap lon

---
+++ btl/view/TBkhac.php
@@ -0,0 +1,166 @@
+<?php require_once "header.php";?>
+<div class="location">
+	<div class="container breadcrumb">
+		<a href="#" title="" class="breadcrumblink"> 
+			<span> Thông báo   >   </span>
+		</a>
+		<a href="TBkhac.php" title="" class="breadcrumblink">
+			<span> Thông báo khác</span>
+		</a>
+	</div>
+	<div class="container detaiduan">
+		<div class="row">
+			<div class="col-lg-8 col-md-7 col-12">
+				<a href="TBkhac.php"><h3 style="color: #003478">Thông báo khác</h3></a>
+				<div class="cardcongtrinh" >
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-4">
+								<img src="../public/img/thong bao.jpg" width="100%" class="img-congtrinhcongbo">
+							</div>
+							<div class="col-lg-8">
+								<a href="#"><h4>Cuộc thi MIS Project 2018</h4></a>
+							</div>
+						</div>
+					</div>
+				<a  href="#" class="d-flex justify-content-end trovedautrang"> > Xem chi tiết</a>
+				</div>
+				<div class="cardcongtrinh">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-4">
+								<img src="../public/img/thong bao.jpg" width="100%" class="img-congtrinhcongbo">	
+							</div>
+							<div class="col-lg-8">
+								<a href="#"><h4>Khóa học Miễn phí APTECH EXPLORER 4.0 cho sinh viên CNTT</h4></a>
+							</div>
+						</div>
+					</div>
+					<a  href="#" class="d-flex justify-content-end trovedautrang"> > Xem chi tiết</a>
+				</div>
+				<div class="cardcongtrinh">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-4">
+								<img src="../public/img/thong bao.jpg" width="100%" class="img-congtrinhcongbo">	
+							</div>
+							<div class="col-lg-8">
+								<a href="#"><h4>Tổ chức gặp mặt và đối thoại với sinh viên năm học 2015 – 2016/h4></a>
+								<p style="text-align: justify;">
+									Nhà trường thông báo kế hoạch tổ chức buổi gặp mặt và đối thoại với sinh viên trong năm học 2015 – 2016 như sau
+								</p>
+							</div>
+						</div>
+					</div>
+					<a  href="#" class="d-flex justify-content-end trovedautrang"> > Xem chi tiết</a>
+				</div>
+				<div class="cardcongtrinh">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-4">
+								<img src="../public/img/thong bao.jpg" width="100%" class="img-congtrinhcongbo">	
+							</div>
+							<div class="col-lg-8">
+								<a href="#"><h4>Lịch trao bằng tốt nghiệp đợt 1, năm học 2015- 2016</h4></a>
+								<p style="text-align: justify;">
+									Sinh viên có mặt tại sảnh T45 trước 8h, gặp trợ lý khoa để nhận trang phục Tốt nghiệp & ổn định vị trí trong hội trường.
+								</p>
+							</div>
+						</div>
+					</div>
+					<a  href="#" class="d-flex justify-content-end trovedautrang"> > Xem chi tiết</a>
+				</div>
+				<div class="cardcongtrinh">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-4">
+								<img src="../public/img/thong bao.jpg" width="100%" class="img-congtrinhcongbo">	
+							</div>
+							<div class="col-lg-8">
+								<a href="#"><h4>Kế hoạch đánh giá điểm rèn luyện học kỳ 1 năm học 2015-2016</h4></a>
+							</div>
+						</div>
+					</div>
+					<a  href="#" class="d-flex justify-content-end trovedautrang"> > Xem chi tiết</a>
+				</div>
+				<div class="cardcongtrinh">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-4">
+								<img src="../public/img/thong bao.jpg" width="100%" class="img-congtrinhcongbo">	
+							</div>
+							<div class="col-lg-8">
+								<a href="#"><h4>Thông báo sinh hoạt lớp tháng 10/2015</h4></a>
+							</div>
+						</div>
+					</div>
+					<a  href="#" class="d-flex justify-content-end trovedautrang"> > Xem chi tiết</a>
+				</div>
+				<div class="cardcongtrinh">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-4">
+								<img src="../public/img/thong bao.jpg" width="100%" class="img-congtrinhcongbo">	
+							</div>
+							<div class="col-lg-8">
+								<a href="#"><h4>Thông báo về việc điều chỉnh kế hoạch tổng kết năm học 2014 - 2015 khối sinh viên</h4></a>
+							</div>
+						</div>
+					</div>
+					<a  href="#" class="d-flex justify-content-end trovedautrang"> > Xem chi tiết</a>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-5 col-12">
+				<h4><a href="thongbao.php" style="color: #0f14a1;">Thông báo</a></h4>
+
+				<ul class="nckh" type="none">
+					<li>
+						<a href="thongbao1.php" title=""> > Thông báo</a>
+					</li>
+					<li> 
+						<a href="TBdaotao.php" title=""> > TB Đào tạo</a>
+					</li>
+					<li>
+						<a href="nghiencuu.php" title=""> > Nghiên cứu khoa học</a>
+					</li>
+					<li> 
+						<a href="tuyendung.php" title=""> > Tuyển dụng</a>
+					</li>
+					<li> 
+						<a href="hocbong.php" title=""> > Học bổng</a>
+					</li>
+					<li> 
+						<a href="TBkhac.php" title=""> > Thông báo khác</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<style type="text/css">
+
+	
+		a.d-flex.justify-content-end.trovedautrang {
+			margin-top: 25px;
+			margin-bottom: 20px;
+			color: #0f14a1;
+		}
+		ul.nckh li a {border-bottom: 1px solid #ccc;display: block;text-decoration: none;padding: 10px 7px;line-height: 2;}
+
+		ul.nckh li :hover {
+			background: #ececec;
+			color: #11497a;
+		}
+		.container.detaiduan {
+			margin-top: 50px;
+		}
+		ul.nckhcntt li {
+			font-size: 16px;
+			color: black;
+			list-style-type: none;
+		}
+	</style>
+</div>
+
+</div>
+<?php require_once "footer.php";?>
+
---
+++ btl/view/nghiencuu.php
@@ -0,0 +1,147 @@
+<?php require_once "header.php";?>
+<div class="location">
+	<div class="container breadcrumb">
+		<a href="#" title="" class="breadcrumblink"> 
+			<span> Thông báo   >   </span>
+		</a>
+		<a href="nghiencuu.php" title="" class="breadcrumblink">
+			<span> Nghiên cứu khoa học</span>
+		</a>
+	</div>
+	<div class="container detaiduan">
+		<div class="row">
+			<div class="col-lg-8 col-md-7 col-12">
+				<a href="nghiencuu.php"><h3 style="color: #003478">Nghiên cứu khoa học</h3></a>
+				<div class="cardcongtrinh" >
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-4">
+								<img src="../public/img/thong bao.jpg" width="100%" class="img-congtrinhcongbo">
+							</div>
+							<div class="col-lg-8">
+								<a href="#"><h4>Kế hoạch tổ chức NCKH sinh viên năm học 2018-2019</h4></a>
+							</div>
+						</div>
+					</div>
+				<a  href="#" class="d-flex justify-content-end trovedautrang"> > Xem chi tiết</a>
+				</div>
+				<div class="cardcongtrinh">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-4">
+								<img src="../public/img/thong bao.jpg" width="100%" class="img-congtrinhcongbo">	
+							</div>
+							<div class="col-lg-8">
+								<a href="#"><h4>Hội nghị khoa học sinh viên lần thứ 31</h4></a>
+							</div>
+						</div>
+					</div>
+					<a  href="#" class="d-flex justify-content-end trovedautrang"> > Xem chi tiết</a>
+				</div>
+				<div class="cardcongtrinh">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-4">
+								<img src="../public/img/thong bao.jpg" width="100%" class="img-congtrinhcongbo">	
+							</div>
+							<div class="col-lg-8">
+								<a href="#"><h4>Thông báo lịch nộp báo cáo Đề tài NCKH sinh viên năm học 2017 - 2018</h4></a>
+							</div>
+						</div>
+					</div>
+					<a  href="#" class="d-flex justify-content-end trovedautrang"> > Xem chi tiết</a>
+				</div>
+				<div class="cardcongtrinh">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-4">
+								<img src="../public/img/thong bao.jpg" width="100%" class="img-congtrinhcongbo">	
+							</div>
+							<div class="col-lg-8">
+								<a href="#"><h4>Kế hoạch tổ chức hội nghị Nghiên cứu khoa học sinh viên khoa CNTT năm học 2017-2018</h4></a>
+							</div>
+						</div>
+					</div>
+					<a  href="#" class="d-flex justify-content-end trovedautrang"> > Xem chi tiết</a>
+				</div>
+				<div class="cardcongtrinh">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-4">
+								<img src="../public/img/thong bao.jpg" width="100%" class="img-congtrinhcongbo">	
+							</div>
+							<div class="col-lg-8">
+								<a href="#"><h4>Hội nghị khoa học sinh viên lần thứ 29</h4></a>
+							</div>
+						</div>
+					</div>
+					<a  href="#" class="d-flex justify-content-end trovedautrang"> > Xem chi tiết</a>
+				</div>
+				<div class="cardcongtrinh">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-4">
+								<img src="../public/img/nckh.jpg" width="100%" class="img-congtrinhcongbo">	
+							</div>
+							<div class="col-lg-8">
+								<a href="#"><h4>Thông báo về Hội nghị Khoa học sinh viên lần thứ 29</h4></a>
+							</div>
+						</div>
+					</div>
+					<a  href="#" class="d-flex justify-content-end trovedautrang"> > Xem chi tiết</a>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-5 col-12">
+				<h4><a href="thongbao.php" style="color: #0f14a1;">Thông báo</a></h4>
+
+				<ul class="nckh" type="none">
+					<li>
+						<a href="thongbao1.php" title=""> > Thông báo</a>
+					</li>
+					<li> 
+						<a href="TBdaotao.php" title=""> > TB Đào tạo</a>
+					</li>
+					<li>
+						<a href="nghiencuu.php" title=""> > Nghiên cứu khoa học</a>
+					</li>
+					<li> 
+						<a href="tuyendung.php" title=""> > Tuyển dụng</a>
+					</li>
+					<li> 
+						<a href="hocbong.php" title=""> > Học bổng</a>
+					</li>
+					<li> 
+						<a href="TBkhac.php" title=""> > Thông báo khác</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<style type="text/css">
+
+	
+		a.d-flex.justify-content-end.trovedautrang {
+			margin-top: 25px;
+			margin-bottom: 20px;
+			color: #0f14a1;
+		}
+		ul.nckh li a {border-bottom: 1px solid #ccc;display: block;text-decoration: none;padding: 10px 7px;line-height: 2;}
+
+		ul.nckh li :hover {
+			background: #ececec;
+			color: #11497a;
+		}
+		.container.detaiduan {
+			margin-top: 50px;
+		}
+		ul.nckhcntt li {
+			font-size: 16px;
+			color: black;
+			list-style-type: none;
+		}
+	</style>
+</div>
+
+</div>
+<?php require_once "footer.php";?>
+
